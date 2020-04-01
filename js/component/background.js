f7d006b024c655ed9471ef5e899e112dd0a77539
Pierre-Philippe Emond <emondpph@gmail.com>
2020-04-01 15:21:23 -0400

    fix license links

--- js/component/background.js
+++ js/component/background.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of the QuidPHP package.
  * Website: https://quidphp.com
- * License: https://github.com/quidphp/lemur/blob/master/LICENSE
+ * License: https://github.com/quidphp/front/blob/master/LICENSE
  */
  
 // background
